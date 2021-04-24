0dc1a3d94fbd0e48425f9fd810c4ba9a0f1a100d
kotharishubh03 <shubh.kothari1111@gmail.com>
2021-04-25 02:50:04 +0530

    added sentiment analysis
    Signed-off-by: kotharishubh03 <shubh.kothari1111@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
-import requests
 import hashlib
-import sqlite3
-import random
-import string
 import json
+import random
+import sqlite3
+import string
+import requests
+from flask import Flask, flash, redirect, render_template, request, url_for
+from tmdbv3api import TMDb
+from tmdbv3api import Movie
+
+tmdb = TMDb()
+tmdb.api_key = 'bf048b0e274f5a9ee17fa19066dfc3ed'
+tmdb.language = 'en'
 
 API_key='bf048b0e274f5a9ee17fa19066dfc3ed'
 def rand_str():
@@ -30,10 +36,12 @@ def get_stats(data):
     gen_origin_dict={"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]}
     gen_dict,rate_dict=dict(),{'0':0,'1':0,'2':0,'3':0,'4':0,'5':0}
     ret=[]
+    if data==[]:
+        return []
     for i in data:
         #print(i[3])
         # 3 for genere if get data changed please change it too
-        gen=i[3].split(';')
+        gen=i['genres'].split(';')
         for j in gen:
             if j!='':
                 if j not in gen_dict:
@@ -42,7 +50,7 @@ def get_stats(data):
                     gen_dict[j] += 1
 
         # 9 for user rating if get data changed please change it too
-        rate=i[9]
+        rate=i['user_review']
         if rate=='':
             rate_dict[0]=rate_dict[0]+1
         else:
@@ -52,7 +60,64 @@ def get_stats(data):
     #print(ret)
     return ret
 
-def get_data_from_API(API_key, Movie_IDs):
+def get_actor(x):
+    movie = Movie()
+    #print(dir(movie))
+    m ,ret_lst= movie.details(int(x)),[]
+    z=m.casts
+    if len(z['cast'])>0:
+        #print(z['cast'][0])
+        if len(z['cast'])>0:
+            top_cast = [0,1,2,3,4,5,6,7,8,9]
+        else:
+            top_cast = [0,1,2,3,4]
+        for i in top_cast:
+            #https://image.tmdb.org/t/p/original
+            temp_lst={}
+            for j in ['name','character','profile_path','id']:
+                if j=='profile_path':
+                    if z['cast'][i][j]!=None:
+                        temp_lst[j]='https://image.tmdb.org/t/p/original'+z['cast'][i][j]
+                    else:
+                        temp_lst[j]='https://webstockreview.net/images/human-clipart-human-symbol-19.png'
+                    continue
+                temp_lst[j]=z['cast'][i][j]
+            #print(temp_lst)
+            ret_lst.append(temp_lst)
+        return ret_lst
+
+def get_comments(Movie_ID): #get individual movie comments from imdb
+    text=[]
+    #must be changed according to ngrok values
+    query = 'http://9b94377b2ebc.ngrok.io/sentimentanalyisis?id='+str(Movie_ID)
+    response =  requests.get(query)
+    if response.status_code==200:
+        array = response.json()
+        return array
+    else:
+        return []
+
+def get_popular_movies():
+    movie = Movie()
+    popular = movie.popular()
+    lst=[]
+    j=0
+    for p in popular:
+        ret_lst={}
+        ret_lst['title']=p.title
+        ret_lst['id']=p.id
+        if p.poster_path==None:
+            ret_lst['poster_path']='https://www.sundialhome.com/assets/images/noImage.jpg'
+        else:
+            ret_lst['poster_path']='https://image.tmdb.org/t/p/original'+p.poster_path
+        ret_lst['popularity']=p.popularity
+        j=j+1
+        lst.append(ret_lst)
+        if j>9:
+            break
+    return lst
+
+def get_data_from_API(API_key, Movie_IDs): # get movie data from api
     text=[]
     data=[]
     user_review=[]
@@ -67,27 +132,28 @@ def get_data_from_API(API_key, Movie_IDs):
             user_review.append(str(i[1]))
     for i in range(len(text)):
         #if change in service_req please change stat to
-        service_req=['title','id','imdb_id','genres','poster_path','popularity','release_date','vote_average','vote_count']
-        lst=[]
-        for req_service in service_req:
+        services=['title','id','imdb_id','genres','tagline','poster_path','popularity','release_date','vote_average','vote_count','overview']
+        lst={}
+        for req_service in services:
             if text[i][req_service] is None:
                 if req_service=='poster_path':
-                    lst.append('https://www.sundialhome.com/assets/images/noImage.jpg')
+                    lst['poster_path']='https://www.sundialhome.com/assets/images/noImage.jpg'
                 else:
-                    lst.append(' ')
+                    lst['poster_path']=' '
             elif req_service=='genres':
                 gen=''
                 for gen_name in text[i]['genres']:
                     gen=gen_name['name']+';'+gen
-                lst.append(gen)
+                lst['genres']=gen
             elif req_service=='poster_path':
-                lst.append(str(posterpath+text[i]['poster_path']))
+                lst['poster_path']=str(posterpath+text[i]['poster_path'])
             else:
-                lst.append(text[i][req_service])
-        lst.append(user_review[i])
+                lst[req_service]=text[i][req_service]
+        lst['user_review']=user_review[i]
         data.append(lst)
             
     return data
+
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -152,9 +218,48 @@ def profile():
         user_data.append(z[0][0])
         user_data.append(z[0][1])
         stats=get_stats(seen_movie_data)
-        #print(stats)
+        popular_movie=get_popular_movies()
         con.commit()
-    return render_template('profile.html',sess_str=sess,data=seen_movie_data,user_data=user_data,stats_gen=json.dumps(stats[0]),stats_rating=json.dumps(stats[1]))
+        if stats==[]:
+            return render_template('profile.html',sess_str=sess,data=seen_movie_data,user_data=user_data,popular_movie=popular_movie)
+        #print(stats[0])
+        else:
+            return render_template('profile.html',sess_str=sess,data=seen_movie_data,popular_movie=popular_movie,user_data=user_data,stats_gen=json.dumps(stats[0]),stats_rating=json.dumps(stats[1]))
+
+@app.route('/movie', methods=['GET','POST'])
+def movie():
+    if request.method=='POST':
+        sess=request.form['sess']
+        comment=request.form['comment']
+        rating=request.form['rating']
+        add_to_watch=request.form['add_to_watch']
+        #print(sess,comment,rating,add_to_watch)
+        con = sqlite3.connect("app.db")
+        cur = con.cursor()
+        z=cur.execute("select user_id,movie_id from user_movie where user_id= (select user_id from user where sess=?) and movie_id = ?",[sess,add_to_watch]).fetchall()
+        if z==[]:
+            cur.execute("insert into user_movie (user_id,movie_id,rateing,comments) values ((select user_id from user where sess=?),?,?,?)",[sess,add_to_watch,rating,comment])
+        else:
+            cur.execute("update user_movie set rateing=? , comments=? where user_id = (select user_id from user where sess=?) and movie_id = ?",[rating,comment,sess,add_to_watch])
+        con.commit()
+        return redirect(url_for('movie',sess=sess,movie_id=add_to_watch))
+    if request.method=='GET':
+        user_data=[]
+        sess=request.args.get('sess')
+        movie_tmbd_id=request.args.get('movie_id')
+        con = sqlite3.connect("app.db")
+        cur = con.cursor()
+        z=cur.execute("select rateing,comments from user_movie where user_id = (select user_id from user where sess= ?) and movie_id = ?",[sess,movie_tmbd_id]).fetchall()
+        if z==[]:
+            z.append([-1])
+        seen_movie_data=get_data_from_API(API_key, [[movie_tmbd_id,z[0][0]]])
+        actor=get_actor(movie_tmbd_id)
+        #print(seen_movie_data[0]['imdb_id'])
+        comments=get_comments(seen_movie_data[0]['imdb_id'])
+        #print(comments)
+        #print(seen_movie_data)
+        con.commit()
+        return render_template('movie.html',sess=sess,seen_movie_data=seen_movie_data[0],actor=actor,comments=comments,user_rate_comm=z)
 
 if __name__=='__main__':
     app.run(debug=True,port=5000,use_reloader=False)
